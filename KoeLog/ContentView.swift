3119030b56279b5badced4cbdf7acf246683c461
Yuto <yuto.200596@gmail.com>
2026-05-24 23:16:34 +0900

    Initial Commit

---
+++ KoeLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KoeLog
+//
+//  Created by 源間悠翔 on 2026/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KoeLog/KoeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoeLogApp.swift
+//  KoeLog
+//
+//  Created by 源間悠翔 on 2026/05/24.
+//
+
+import SwiftUI
+
+@main
+struct KoeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
